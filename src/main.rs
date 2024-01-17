ffd7b8be82ff3b1ea26584476e161873ddea637c
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2024-01-16 21:21:23 -0600

    finished

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@
 // ip_sniffer.exe 192.168.1.1
 
 use std::env;
-use std::net::IpAddr;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
 use std::process;
 use std::str::FromStr;
 use std::sync::mpsc::{channel, Sender};
 use std::thread;
 
+const MAX: u16 = 65535;
 struct Arguments {
     flag: String,
     ipaddr: IpAddr,
@@ -60,15 +62,61 @@ impl Arguments {
     }
 }
 
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    
+    let mut port: u16 = start_port + 1; 
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+
+        if (MAX - port) < num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-    let arguments = Arguments::new(&args).unwrap_or_else(|err| {
-        if err.contains("help") {
-            process::exit(0);
-        } else {
-            eprintln!("{} problem parsing arguments: {}", program, err);
-            process::exit(0);
+    let arguments = Arguments::new(&args).unwrap_or_else(
+        |err| {
+            if err.contains("help") {
+                process::exit(0);
+            } else {
+                eprintln!("{} problem parsing arguments: {}", program, err);
+                process::exit(0);
+            }
         }
-    });
+    );
+    let num_threads: u16 = arguments.threads;
+    let addr = arguments.ipaddr;
+    let(tx, rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, addr, num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+
+    for p in rx {
+        out.push(p);
+    }
+    println!("");
+
+    out.sort();
+
+    for v in out {
+        println!("{} is open", v);
+    }
 }
